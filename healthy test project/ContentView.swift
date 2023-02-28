2e3e0dbc372a88ba24af1414cb3d24905695d9d8
Sina <registry123456@gmail.com>
2023-02-28 20:43:38 +0330

    Initial Commit

---
+++ healthy test project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  healthy test project
+//
+//  Created by sina taherkhani on 2/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ healthy test project/healthy_test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthy_test_projectApp.swift
+//  healthy test project
+//
+//  Created by sina taherkhani on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct healthy_test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
